3444f15a776b2a55335fc687c92e700c3266d80a
TJS-95 <tjs_95@163.com>
2021-05-11 20:57:30 +0800

    数据存储

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import superagent from 'superagent'
 import cherrio from 'cheerio'
 
@@ -6,9 +8,25 @@ interface Course {
   count: number
 }
 
+interface courseResult {
+  time: number
+  data: Course[]
+}
+
+interface Content {
+  [propName: number]: Course[]
+}
+
 class Crowller {
   private secret = 'x3b174jsx'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private filePath = path.resolve(__dirname, '../data/course.json')
+
+  /**
+   * 解析html
+   * @param html
+   * @returns
+   */
   getCourseInfo(html: string) {
     const $ = cherrio.load(html)
     const courseItem = $('.course-item')
@@ -23,16 +41,36 @@ class Crowller {
       time: new Date().getTime(),
       data: courseInfos,
     }
-    console.log(result)
+    return result
   }
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.getCourseInfo(result.text)
+    return result.text
+  }
+
+  /**
+   * 存储
+   * @param courseInfo
+   */
+  generateJsonContent(courseInfo: courseResult) {
+    let fileContent: Content = {}
+    if (fs.existsSync(this.filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
+  }
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml()
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo)
+    fs.writeFileSync(this.filePath, JSON.stringify(fileContent))
   }
 
   constructor() {
-    this.getRawHtml()
+    this.initSpiderProcess()
   }
 }
 
