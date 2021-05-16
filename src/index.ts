67ac31546fe20256646b0303d03f3146b27996b7
TJS-95 <tjs_95@163.com>
2021-05-16 22:40:16 +0800

    express 实现数据返回

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cookieSession from 'cookie-session'
+import router from './router'
+
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['tan'],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+)
+
+app.use(router)
+
+app.listen(7001, () => {
+  console.log('http://localhost:7001')
+})
--- src/utils/crowller.ts
+++ src/utils/crowller.ts
@@ -1,14 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 import superagent from 'superagent'
-import DellAnalyzer from './anzlyzer'
 
 export interface Analyzer {
   analyze: (html: string, filePaht: string) => string
 }
 
 class Crowller {
-  private filePath = path.resolve(__dirname, '../data/course.json')
+  private filePath = path.resolve(__dirname, '../../data/course.json')
 
   private async getRawHtml() {
     const result = await superagent.get(this.url)
@@ -30,8 +29,4 @@ class Crowller {
   }
 }
 
-const secret = 'x3b174jsx'
-const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
-
-const analyzer = DellAnalyzer.getInstance()
-const crowller = new Crowller(url, analyzer)
+export default Crowller
